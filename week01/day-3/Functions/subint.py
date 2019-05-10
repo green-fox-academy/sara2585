29653de09b579dc40d305c00fd4cc07fcaf73525
Sara Yu <Sara_Yu@Epam.com>
2019-05-10 15:09:50 +0800

    modify subint.py with the help of Levi

--- week01/day-3/Functions/subint.py
+++ week01/day-3/Functions/subint.py
@@ -6,24 +6,16 @@
 #  Or returns an empty list if the number is not part of any of the numbers in the list
 
 
-def subint(a, *args):
-    ind = []
-    
-    for i in args:
-        
-        
-        if a in listi:
-            n = listi.index(a)
-            print(n)
-            ind.append(int(n))
-    return ind
+def subint(a, list):
+    indices = []
+    for i in range(len(list)):
+        if str(a) in str(list[i]):
+            indices.append(i)
+    return indices
 
 # Example
-
 print(subint(1, [1, 11, 34, 52, 61]))
-
 # should print: `[0, 1, 4]`
-
 print(subint(9, [1, 11, 34, 52, 61]))
 
 # should print: '[]'
