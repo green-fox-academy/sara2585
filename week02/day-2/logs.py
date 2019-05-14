6f55da0ba8d24486d267ed871ef8729996617c8d
Sara Yu <Sara_Yu@Epam.com>
2019-05-14 19:03:52 +0800

    add logs.py

---
+++ week02/day-2/logs.py
@@ -0,0 +1,35 @@
+# Read all data from 'log.txt'.
+# Each line represents a log message from a web server
+# Write a function that returns an array with the unique IP adresses.
+# Write a function that returns the GET / POST request ratio.
+import re
+r = re.compile(r"[\d]{2}\.[\d]{2,3}\.[\d]{2}\.[\d]{2}")
+
+
+f = open("C:\\Users\\Sara_Yu\\Desktop\\Sara\\log.txt", "r")
+contents = f.read()
+#print(contents)
+
+def catch_IP(contents):
+    r = re.compile(r"[\d]{2}\.[\d]{2,3}\.[\d]{2}\.[\d]{2}")
+    f = open("C:\\Users\\Sara_Yu\\Desktop\\Sara\\log.txt", "r")
+    contents = f.read()
+    return r.findall(contents)
+
+
+
+print(catch_IP(contents))
+
+def catch_request(contents):
+    r = re.compile(r"POST")
+    p = len(r.findall(contents))
+    g =len(re.compile(r"GET").findall(contents))
+    return f"the ratio for GET is {g/(g+p)}, the ratio for POST is {p/(g+p)}"
+    
+
+print(catch_request(contents))
+    
+
+
+
+
