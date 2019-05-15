32de05f28b6b9c4dd2e459d5cdaaf8774fcd9070
Sara Yu <Sara_Yu@Epam.com>
2019-05-15 22:31:36 +0800

    Exam performance.py

--- week02/day-3/Exam performance.py
+++ week02/day-3/Exam performance.py
@@ -2,36 +2,57 @@ import csv
 import re
 #r1 = re.compile(r"(?P<hour>[\d]+)?h?\:?(?P<minute>[\d]+)?m?\:?(?P<second>[\d]+)?s?")
 h = re.compile(r"^([\d]*\.?[\d]+)[h,:]")
-m = re.compile(r"(([\d]+)m|:([\d]+):)")
+m = re.compile(r"([\d]+)m|:([\d]+):")
 s = re.compile(r"([\d]+)s|:([\d]+)$")
-#r2 = re.compile(r"(\.[\d]+)h")
 
-
-
-f = open("C:\\Users\\Sara_Yu\\Desktop\\Sara\\greenfox\\sara_repo\\week02\\day-3\\exams.tsv", 'r')
+f = open("exams.tsv", 'r')
 tsv_contents  = csv.reader(f)
 tsv = []
 for line in tsv_contents:
     tsv.append(line[0].split("\t"))
-
+ratios = []
 for i in range(1,len(tsv)):
     hours = h.match(tsv[i][2])
     minutes = m.search(tsv[i][2])
     seconds = s.search(tsv[i][2])
     sum = 0.00
+    #print(minutes.group(1))
+    if hours != None:
+      #print(hours.group(1))
+      hours = float(hours.group(1))*60
+      sum += hours
+    if seconds != None:
+      if seconds.group(1) != None:
+        seconds = float(seconds.group(1))/60
+      else:
+        seconds = float(seconds.group(2))/60
+      sum += seconds
+    if minutes != None:
+      if minutes.group(1) != None:
 
- #   if hours.group(1):
-   #     hours = float(hours.goup(1))*60
-   #     sum += hours
-   # if seconds:
-    #    seconds = float(seconds.group(1))/60
-    #    sum += seconds
-   # if minutes:
-     #   minutes = float(minutes)
-     #   sum += minutes
+        minutes = float(minutes.group(1))
+      else:
+        minutes = float(minutes.group(2))
+      sum += minutes
+    if sum != 0:
+      ratio = int(tsv[i][1])/sum
+    ratios.append(ratio)
+
 
         
-    print(sum)
+#print(ratios)
+highest_ratio = 0
+for i in range(len(ratios)):
+  if ratios[i] > highest_ratio:
+    highest_ratio = ratios[i]
+location = 0
+for i in range(len(ratios)):
+  if ratios[i] == highest_ratio:
+    location = i+1
+
+
+print(f"The highest points/mins ratio is: {highest_ratio}")
+print(f"The highest points/mins ratio located in {tsv[location]}")
 
 
 
