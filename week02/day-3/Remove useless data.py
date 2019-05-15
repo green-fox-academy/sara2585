1a2d35c9ef901b17e952b664792c7ed2ecef8620
Sara Yu <Sara_Yu@Epam.com>
2019-05-15 13:02:30 +0800

    modified find_oldest_movie.py and remove useless data.py

--- week02/day-3/Remove useless data.py
+++ week02/day-3/Remove useless data.py
@@ -1,17 +1,25 @@
 import csv
-f = open("C:\\Users\\Sara_Yu\\Desktop\\Sara\\greenfox\\sara_repo\\week02\\day-3\\election.csv", 'r')
-contents = list(csv.reader(f))
 
 def remove_useless_data(contents):
     for i in contents:
         i = list(str(i).split(","))
-
+     #   print(i)
+     
+    contents_new = []
     for i in contents:
-        if i[0] == "" or i[1] == "" or i[3] == "":
+        if len(i) < 4:
             print(i)
-            contents.remove(i)
+            contents_new.append(i)
+        elif i[0] == "" or i[1] == "" or i[3] == "":
+            print(i)
+            contents_new.append(i)
+    for i in contents_new:
+        contents.remove(i)
+            
 
- 
+f = open("C:\\Users\\Sara_Yu\\Desktop\\Sara\\greenfox\\sara_repo\\week02\\day-3\\election.csv", 'r')
+contents = list(csv.reader(f))
 remove_useless_data(contents)
+f.close()
 
     
--- week02/day-3/find_oldest_movie.py
+++ week02/day-3/find_oldest_movie.py
@@ -1,19 +1,21 @@
 
 import csv
-f = open("C:\\Users\\Sara_Yu\\Desktop\\Sara\\greenfox\\sara_repo\\week02\\day-3\\movies.csv", "r")
-contents = list(csv.reader(f))
 
-def find_oldest_movie(contents):
-    
-    for i in contents:
-        i = (str(i).split(";"))
+def find_oldest_movie(csv_contents):
+    contents1 = []
+    for i in csv_contents:
+        i = list(str(i).split(";"))
+        contents1.append(i)
 
-    min = contents[0]
-    for i in contents:
-        if i[0][1] < min[0][1]:
+    min = contents1[0]
+
+    for i in contents1:
+        if i[1] < min[1]:
             min = i  
     print(min)
 
-
+f = open("C:\\Users\\Sara_Yu\\Desktop\\Sara\\greenfox\\sara_repo\\week02\\day-3\\movies.csv", "r")
+contents = list(csv.reader(f))
 find_oldest_movie(contents)
+f.close()
 
