0371d8884fcbd1ce7bafebfc9f9bc86fc16aa347
Sara Yu <Sara_Yu@Epam.com>
2019-05-22 15:28:21 +0800

    add new_movienavigate.py

--- week03/day-3/new_movienavigate.py
+++ week03/day-3/new_movienavigate.py
@@ -12,5 +12,9 @@ def movie_navigate():
 
 @app.route('/movie/<movie_id>')
 def my_firstwebsite(movie_id):
-    movie_dic = {"01": "movie1.html", "02":"movie2.html", "03": "movie3.html", "04": "movie4.html", "05": "movie5.html"}   
-    return render_template(movie_dic[movie_id])
+    f = open(f"{movie_id}.txt") 
+    contents = f.readlines()
+    title = contents[0]
+    description = contents[1]
+    pic = contents[2]
+    return render_template("index.html", title = title, description = description, pic = pic)
