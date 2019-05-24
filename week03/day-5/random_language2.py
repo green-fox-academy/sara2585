03aeaffdc789958ee86318048cce6c77cb8ac873
Sara Yu <Sara_Yu@Epam.com>
2019-05-24 10:27:09 +0800

    add random_language2.py

---
+++ week03/day-5/random_language2.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import render_template
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_john():
+    greetings = ['Hello,', 'Hi, ', 'How are you today?']
+    names = ['John', 'Lucy', 'David']
+    greeting = random.choice(greetings)
+    name = random.choice(names)
+    return render_template('Greet_John2.html', greeting = greeting, name = name)
