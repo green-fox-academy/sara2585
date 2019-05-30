5846d7fd35af27b56a3ad1c89f0caaa5ef599225
Sara Yu <Sara_Yu@Epam.com>
2019-05-30 23:57:39 +0800

    delete test files

--- week04/day-04/test.py
+++
@@ -1,8 +0,0 @@
-
-
-if 'reactions' in history:
-        reactions = history['reactions']
-        for reaction in reactions:
-            users = reaction['users']
-            for user in users:
-                if user != []:
